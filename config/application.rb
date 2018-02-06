75640f4f25c181ce69d2558161c38e014251b13c
[pan16] <[mnsaabc@yahoo.co.jp]>
2018-02-06 16:54:34 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Uploader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.
-    config.assets.initialize_on_precompile = false
+    config.load_defaults 5.1
+    config.assets.compile= false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
